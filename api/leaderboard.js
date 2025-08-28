544dd5949c600a2d93c95841401d061f70326a91
Mario Nogueira Bouzas <mario.nogueira@imatia.com>
2025-08-28 23:21:58 +0200

    Add CORS headers to all API endpoints

--- api/leaderboard.js
+++ api/leaderboard.js
@@ -6,6 +6,16 @@ const redis = new Redis({
 });
 
 export default async function handler(req, res) {
+  // Configurar CORS
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  
+  // Manejar preflight OPTIONS
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  
   // Solo permitir GET
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Method not allowed' });
